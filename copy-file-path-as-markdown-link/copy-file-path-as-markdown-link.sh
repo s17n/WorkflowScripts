2e5286efc88a17975969fce9b99cfcc4a0edfa40
s17n <wusterhausen@googlemail..com>
2025-02-01 12:56:00 +0100

    Added app name

--- copy-file-path-as-markdown-link/copy-file-path-as-markdown-link.sh
+++ copy-file-path-as-markdown-link/copy-file-path-as-markdown-link.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 
 filePath="$1"
+app=$(xattr -p s17n.app $1)
 # echo $filePath >> ~/Projects/Workflow-Tools/copy-file-path-as-markdown-link/debug.log
-echo "[Screenshot](file:"$filePath")" | pbcopy
+echo "[Screenshot - "$app"](file:"$filePath")" | pbcopy
